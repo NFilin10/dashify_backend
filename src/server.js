bf069b254bb30a53db27285b8dc529014d7ddd4c
Nikita Filin <nikita10filin@gmail.com>
2025-04-11 17:09:42 +0300

    auth logic added

---
+++ src/server.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const cors = require('cors')
+const authRoute = require('./routes/auth.route')
+const cookieParser = require('cookie-parser');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 8080;
+const app = express();
+app.set("trust proxy", 1);
+
+const corsOptions ={
+    credentials: true,
+    optionSuccessStatus: 200,
+    origin: true,
+}
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/auth/', authRoute);
+
+const server = app.listen(port, () => {
+    console.log("Server is listening to port " + port);
+});
+
+module.exports = server;
